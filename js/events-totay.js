996ceb5bff6cf0b0ea599a719837bbadae99132b
aarafat <amirarafat1@gmail.com>
2017-02-02 19:19:04 -0700

    updated the data files to latest.

---
+++ js/events-totay.js
@@ -0,0 +1,51 @@
+var today = [
+  {
+    "Day": "Friday",
+    "Event1": "Khutbah - 1pm",
+    "Event2": "Quran Word by Word” After Isha",
+    "Event3": "",
+    "Event4": ""
+  },
+  {
+    "Day": "Saturday",
+    "Event1": "Quran School - 9am",
+    "Event2": "Sisters Halaqa - 2pm",
+    "Event3": "Social/Family Events TBD",
+    "Event4": ""
+  },
+  {
+    "Day": "Sunday",
+    "Event1": "Fajr Halaqa/Breakfast",
+    "Event2": "Sunday School 9:20am",
+    "Event3": "درس في التفسير باللغة العربية بعد العشاء",
+    "Event4": ""
+  },
+  {
+    "Day": "Monday",
+    "Event1": "Lil' Ummah - 9:15am",
+    "Event2": "ESL for Sisters - 9:30",
+    "Event3": "Quran School - 5:30",
+    "Event4": ""
+  },
+  {
+    "Day": "Tuesday",
+    "Event1": "Sisters Tajweed-11am",
+    "Event2": "Quran School - 5:30",
+    "Event3": "",
+    "Event4": ""
+  },
+  {
+    "Day": "Wednesday",
+    "Event1": "Lil' Ummah - 9:15am",
+    "Event2": "ESL for Sisters - 9:30",
+    "Event3": "درس للأخوات - 11AM",
+    "Event4": "Quran School - 5:30pm"
+  },
+  {
+    "Day": "Thursday",
+    "Event1": "Srs. Tajweed-10am",
+    "Event2": "Quran School- 5:30",
+    "Event3": "Halaqa on Adhkar after Isha",
+    "Event4": ""
+  }
+];
--- js/iqama-data.js
+++ js/iqama-data.js
@@ -1,4 +1,289 @@
 var iqamaNew = [
+  {
+    "Month": "March",
+    "Day": "1",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "2",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "3",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "4",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "5",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "6",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "7",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "8",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "9",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "10",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "11",
+    "Fajr Iqama": "5:45",
+    "Dhuhr Iqama": "12:30",
+    "Asr  Iqama": "3:45",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "7:30"
+  },
+  {
+    "Month": "March",
+    "Day": "12",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "13",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "14",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "15",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "16",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "17",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "18",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "19",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "20",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "21",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "22",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "23",
+    "Fajr Iqama": "6:15",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "8:45"
+  },
+  {
+    "Month": "March",
+    "Day": "24",
+    "Fajr Iqama": "6:00",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "9:00"
+  },
+  {
+    "Month": "March",
+    "Day": "25",
+    "Fajr Iqama": "6:00",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "9:00"
+  },
+  {
+    "Month": "March",
+    "Day": "26",
+    "Fajr Iqama": "6:00",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "9:00"
+  },
+  {
+    "Month": "March",
+    "Day": "27",
+    "Fajr Iqama": "6:00",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "9:00"
+  },
+  {
+    "Month": "March",
+    "Day": "28",
+    "Fajr Iqama": "6:00",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "9:00"
+  },
+  {
+    "Month": "March",
+    "Day": "29",
+    "Fajr Iqama": "6:00",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "9:00"
+  },
+  {
+    "Month": "March",
+    "Day": "30",
+    "Fajr Iqama": "6:00",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "9:00"
+  },
+  {
+    "Month": "March",
+    "Day": "31",
+    "Fajr Iqama": "6:00",
+    "Dhuhr Iqama": "1:30",
+    "Asr  Iqama": "5:00",
+    "Maghrib Iqama": "10 Min after Adhan",
+    "Isha  Iqama": "9:00"
+  }
+];
+
+
+// -----------------------------------------------------------------------------
+
+var iqamaOld = [
 	{
 		"Month": "December",
 		"Day": "1",
@@ -278,333 +563,4 @@ var iqamaNew = [
 		"Maghrib Iqama": "10 Min after Adhan",
 		"Isha  Iqama": "7:30"
 	}
-];
-
-
-// -----------------------------------------------------------------------------
-
-var iqamaOld = [
-  {
-    "Month": "November",
-    "Day": 1,
-    "Fajr Iqama": "6:15",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "4:30",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "8:00"
-  },
-  {
-    "Month": "November",
-    "Day": 2,
-    "Fajr Iqama": "6:15",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "4:30",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "8:00"
-  },
-  {
-    "Month": "November",
-    "Day": 3,
-    "Fajr Iqama": "6:15",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "4:30",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "8:00"
-  },
-  {
-    "Month": "November",
-    "Day": 4,
-    "Fajr Iqama": "6:15",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "4:30",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "8:00"
-  },
-  {
-    "Month": "November",
-    "Day": 5,
-    "Fajr Iqama": "6:15",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "4:30",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "8:00"
-  },
-  {
-    "Month": "November",
-    "Day": 6,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 7,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 8,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 9,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 10,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 11,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 12,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 13,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 14,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 15,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 16,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 17,
-    "Fajr Iqama": "5:30",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 18,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 19,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 20,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 21,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 22,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 23,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 24,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:15",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 25,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:00",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 26,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:00",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 27,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:00",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 28,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:00",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 29,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:00",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-  {
-    "Month": "November",
-    "Day": 30,
-    "Fajr Iqama": "5:45",
-    "Dhuhr Iqama": "12:30",
-    "Asr  Iqama": "3:00",
-    "Maghrib Iqama": "10 Min after Adhan",
-    "Isha  Iqama": "7:30"
-  },
-]
-
-// added for backup
-var today = [
-  {
-    "Day": "Friday",
-    "Event1": "Khutbah - 1pm",
-    "Event2": "Quran Word by Word” After Isha",
-    "Event3": "",
-    "Event4": ""
-  },
-  {
-    "Day": "Saturday",
-    "Event1": "Quran School - 9am",
-    "Event2": "Sisters Halaqa - 2pm",
-    "Event3": "Social/Family Events TBD",
-    "Event4": ""
-  },
-  {
-    "Day": "Sunday",
-    "Event1": "Fajr Halaqa/Breakfast",
-    "Event2": "Sunday School 9:20am",
-    "Event3": "درس في التفسير باللغة العربية بعد العشاء",
-    "Event4": ""
-  },
-  {
-    "Day": "Monday",
-    "Event1": "Lil' Ummah - 9:15am",
-    "Event2": "ESL for Sisters - 9:30",
-    "Event3": "Quran School - 5:30",
-    "Event4": ""
-  },
-  {
-    "Day": "Tuesday",
-    "Event1": "Sisters Tajweed-11am",
-    "Event2": "Quran School - 5:30",
-    "Event3": "",
-    "Event4": ""
-  },
-  {
-    "Day": "Wednesday",
-    "Event1": "Lil' Ummah - 9:15am",
-    "Event2": "ESL for Sisters - 9:30",
-    "Event3": "درس للأخوات - 11AM",
-    "Event4": "Quran School - 5:30pm"
-  },
-  {
-    "Day": "Thursday",
-    "Event1": "Srs. Tajweed-10am",
-    "Event2": "Quran School- 5:30",
-    "Event3": "Halaqa on Adhkar after Isha",
-    "Event4": ""
-  }
 ];
