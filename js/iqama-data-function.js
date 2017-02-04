512ce136054e112ea1a2a18986f6bf0beac6a51a
aarafat <amirarafat1@gmail.com>
2017-02-03 19:48:30 -0700

    cleaned up the function to not repeat and using map to iterate.

--- js/iqama-data-function.js
+++ js/iqama-data-function.js
@@ -1,60 +1,35 @@
 jQuery(document).ready(function( $ ) {
-	var iqamaNewTimes = iqamaNew;
-	var iqamaOldTimes = iqamaOld;
-	var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	var currentDate = new Date();
-	var currentMonth = monthNames[currentDate.getMonth()];
-	var formattedDay = currentDate.getDay();
-	var formattedDate = currentDate.getDate();
-	var formattedMonth = currentDate.getMonth();
-	var formattedYear = currentDate.getFullYear();
-	$('#prayer-times').append('<h3>Iqama Times for ' + currentMonth + '</h3>');
+    var iqamaNewData = iqamaNew;
+	var iqamaOldData = iqamaOld;
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var currentDate = new Date();
+    var currentMonth = monthNames[currentDate.getMonth()];
+    var formattedDayNumber = currentDate.getDate();
+    $('#prayer-times').append('<h3>Iqama Times for ' + currentMonth + '</h3>');
 	$('#prayer-times').append('<ul></ul>');
-
-	function loopOld () {
-		for( var i = 0; i < iqamaOldTimes.length; i++ ) {
-			var obj = iqamaOldTimes[i];
-
-			for (var key in obj ) {
-				if (currentMonth === obj.Month) {
-					if (!$('#prayer-times ul li.index'+i).length) {
-						$('#prayer-times ul').append('<li class="clearfix index'+i+'"><span class="month">' + obj.Month +' </span></li>');
-					}
-					if (key === 'Day') {
-						if (formattedDate === Number(obj.Day)) {
-							$('#prayer-times ul li.index' + i).append('<span class="day">'+ obj.Day + ' </span>').css('background-color', '#ddd');
-						} else {
-							$('#prayer-times ul li.index' + i).append('<span class="day">'+ obj.Day + ' </span>');
-						}
-					} else if (key !== 'Month' && key !== 'Day') {
-						$('#prayer-times ul li.index' + i).append('<span class="prayerTime">' + key + ' </span><span class="prayerTime">' + obj[key] + ' </span>');
-					}
-				}
-			}
-		}
-	}
-	function loopNew () {
-		for( var i = 0; i < iqamaNewTimes.length; i++ ) {
-			var obj = iqamaNewTimes[i];
-	
-			for (var key in obj ) {
-				if (currentMonth === obj.Month) {
-					if (!$('#prayer-times ul li.index'+i).length) {
-						$('#prayer-times ul').append('<li class="clearfix index'+i+'"><span class="month">' + obj.Month +' </span></li>');
-					}
-					if (key === 'Day') {
-						if (formattedDate === Number(obj.Day)) {
-							$('#prayer-times ul li.index' + i).append('<span class="day">'+ obj.Day + ' </span>').css('background-color', '#ddd');
-						} else {
-							$('#prayer-times ul li.index' + i).append('<span class="day">'+ obj.Day + ' </span>');
-						}
-					} else if (key !== 'Month' && key !== 'Day') {
-						$('#prayer-times ul li.index' + i).append('<span class="prayerTime">' + key + ' </span><span class="prayerTime">' + obj[key] + ' </span>');
-					}
-				}
-			}
-		}
-	}
-	loopOld();
-	loopNew();
+    function createIqamaCalendar(iqamaTimes) {
+        var prayerSchedule = iqamaTimes.map(function(iqamaToday, i) {
+            var currentDayProperties = Object.getOwnPropertyNames(iqamaToday);
+            currentDayProperties.map(function(prop){
+                if (prop === "Month") {
+                    $('#prayer-times ul').append('<li class="clearfix index'+i+'"><span class="month">' + iqamaToday.Month +' </span></li>');
+                }
+                if (prop === "Day" && formattedDayNumber === Number(iqamaToday.Day)) {
+                    $('#prayer-times ul li.index' + i).append('<span class="day">'+ iqamaToday.Day + ' </span>').css('background-color', '#ddd');
+                } else if (prop === "Day" && formattedDayNumber !== iqamaToday.Day) {
+                    $('#prayer-times ul li.index' + i).append('<span class="day">'+ iqamaToday.Day + ' </span>');
+                }
+                if (prop === "Maghrib Iqama") {
+                    $('#prayer-times ul li.index' + i).append('<span class="prayerTime magrib">' + prop + ' </span><span class="prayerTime">' + iqamaToday[prop] + ' </span>');
+                } else if (prop !== "Day" && prop !== "Month") {
+                    $('#prayer-times ul li.index' + i).append('<span class="prayerTime">' + prop + ' </span><span class="prayerTime">' + iqamaToday[prop] + ' </span>');
+                }
+            });
+        });
+    }
+    if (currentMonth === iqamaNewData[0].Month) {
+        createIqamaCalendar(iqamaNewData);
+    } else {
+        createIqamaCalendar(iqamaOldData);
+    }
 });
